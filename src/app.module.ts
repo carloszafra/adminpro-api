87e2f2a8a048fcc7b7e03ed8a76b44a6e44315fd
carlos <cjzafrar@gmail.com>
2020-11-16 16:25:29 -0400

    SPA first deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config'; 
-
-
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 
 import { AppController } from './app.controller';
@@ -16,6 +16,9 @@ import { SearchModule } from './search/search.module';
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public')
+    }),
     MongooseModule.forRoot(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}${process.env.MONGO_URL}`),
     UsersModule,          
     AuthModule,
